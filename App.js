a0e5bd39bab46adc70691f65f43edc63234251d7
LEGEND PULKIT <58383745+LEGENDPULKIT@users.noreply.github.com>
2020-12-24 22:13:10 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './screens/LoginScreen'
+
+export default function App() {
+  return (
+    <LoginScreen/>
+  );
+}
+
